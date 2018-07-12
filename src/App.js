60196b38557eed3254a7394a98dcabd3b24ce975
Vladislav1552 <vladiskav333@gmail.com>
2018-07-12 20:37:53 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ import SecondSlide from './SecondSlide/SecondSlide.js';
 import ThirdSlide from './ThirdSlide/ThirdSlide.js';
 import video from './video/videoplayback.mp4'
 import FourthSlide from './FourthSlide/FourthSlide.js'
+
 class App extends Component {
-  constructor(props) {
+ /* constructor(props) {
     super(props);
     this.state = {
-      animatedClass: '',
       isHide: false,
-      ReactGame: false,
-      ReactButton: []
     }
     this.hideBar = this.hideBar.bind(this)
   }
+
   hideBar() {
     let { isHide } = this.state
     window.scrollY < this.prev ?
@@ -26,6 +25,8 @@ class App extends Component {
       isHide && this.setState({ isHide: false })
     this.prev = window.scrollY;
   }
+  */
+
   componentDidMount() {
     $(document).ready(function () {
       $('a[data-translate-key="Feedback"]').click(function () {
@@ -42,14 +43,14 @@ class App extends Component {
           })
         });
         */
-      $(window).scroll(function () {
+   $(window).scroll(function () {
         if ($(window).scrollTop() > 120) {
           $('.App__2st-slide_imageHtmlCssJs').fadeIn(2000) && $('.App__2st-slide_about').fadeIn(2000);
         } else {
           $('.App__2st-slide_imageHtmlCssJs').fadeOut(400) && $('.App__2st-slide_about').fadeOut(400);
         }
-           if ($(window).scrollTop() > 450) {
-      
+        if ($(window).scrollTop() > 450) {
+
           $('.App__3st-slide_skill').fadeIn(600)
         } else {
           $('.App__3st-slide_skill').fadeOut(400)
@@ -59,29 +60,26 @@ class App extends Component {
 
         } else {
           $('.Menu').css({ 'display': 'none' })
-
         }
-     
       });
     });
-    window.addEventListener('scroll', this.hideBar);
+
+  window.addEventListener('scroll', this.hideBar);
   }
+
   componentWillUnmount() {
     window.removeEventListener('scroll', this.hideBar);
   }
 
-  render() {
-
-
-    
-    let classHide = this.state.isHide ? "hide" : ""
-    return (
-      <div className="App">
-        <FirstSlide />
-        <SecondSlide />
-        <ThirdSlide />
-        <FourthSlide />
-      </div>
+     render() {
+    //let classHide = this.state.isHide ? "hide" : ""
+        return (
+         <div className="App">
+           <FirstSlide />
+           <SecondSlide />
+           <ThirdSlide />
+           <FourthSlide />
+         </div>
     );
   }
 }
